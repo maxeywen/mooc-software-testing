7a1c115bef59b6aea9b42330a806a20c61e6fe31
wen <me@abc.com>
2018-11-08 16:49:43 +0800

    Fixed Numfinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -8,7 +8,7 @@ public class NumFinderMain {
         // this works
         // nf.find(new int[] {4, 25, 7, 9});
 
-        // this crashes
+
         nf.find(new int[] {4, 3, 2, 1});
 
         System.out.println(nf.getLargest());
